91be123982c2aa58a40fd60b8c2dc5e1987150e6
faten <hadjmansour.faten@gmail.com>
2017-01-15 21:19:31 +0100

    remove

--- fokkus/README.md
+++
@@ -1,4 +0,0 @@
-fokkus
-======
-
-A Symfony project created on December 3, 2016, 9:27 pm.
--- fokkus/src/Fokkus/V1Bundle/FokkusV1Bundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fokkus\V1Bundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FokkusV1Bundle extends Bundle
-{
-}
